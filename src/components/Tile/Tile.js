fc005f1fceb63454fe6cf71bd6243036c182e182
Angela Maugey <angela.maugey@kinandcarta.com>
2022-09-01 11:53:12 +0100

    Completed module 3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,11 +3,11 @@ import React from 'react'
 import './Tile.css'
 
 const Tile = (props) => {
-  const { selected, matched, color } = props
+  const { selected, matched, color, handleTileClicked, id } = props
   
   const generatedColor = selected || matched ? ({ backgroundColor: color }) : null
   return (
-    <div className='Tile' style={generatedColor}>
+    <div className='Tile' style={generatedColor} onClick={() => handleTileClicked(id, color)}>
       {selected || matched ? <props.svg /> : null}
     </div>
   )
